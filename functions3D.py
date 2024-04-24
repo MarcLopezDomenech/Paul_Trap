2334f1c7e3e3d90dd2f3910315c1f32ebbd218a4
Diego Zafra <diego.zafra@estudiantat.upc.edu>
2024-04-24 21:22:53 +0200

    Error correction
    Delete the main, that shouldn't be there, correct some errors and change the word mesh by obj

--- functions3D.py
+++ functions3D.py
@@ -126,43 +126,43 @@ if __name__ == '__main__':
 
     plot_mesh(obj)
 
-def linear_system(mesh: object, V: np.array) -> np.array:
+def linear_system(obj: object, V: np.array) -> np.array:
     '''
-    mesh: object qith the mesh of all the surfaces
+    mesh: object with the mesh of all the surfaces
     V: np.array of size N, tells the potential in each mesh point
 
     Returns an array of size N with the surface charge density in each mesh point
     '''
-    M=len(mesh.bari)
-    N = len(mesh.vertex)
+    M=len(obj.bari)
+    N = len(obj.vertex)
     Z = np.zeros((N, M))
-    for i in range(M - 1):
-        vert=mesh.topol(i)
-        v1=mesh.vertex(vert(0))
-        v2=mesh.vertex(vert(1))
-        v3=mesh.vertex(vert(2))
-        Z[:,i]=vec_func(mesh.bari,v1, v2, v3)
+    for i in range(M):
+        vert=obj.topol[i]
+        v1=obj.vertex(vert[0])
+        v2=obj.vertex(vert[1])
+        v3=obj.vertex(vert[2])
+        Z[:,i]=vec_func(obj.bari,v1, v2, v3)
     Z=Z/4*pi*epsilon
     b = V
     q = np.linalg.solve(Z, b)
     return q
 
-def pot_surf(mesh: object,V0: np.array) -> np.array:
+def pot_surf(obj: object,V0: np.array) -> np.array:
     '''
-    mesh: object qith the mesh of all the surfaces
+    mesh: object with the mesh of all the surfaces
     V0: np.array, with the potentials at each surface
 
     Returns an array of size M (number of baricenters) with the potential at each baricenter
     '''
-    surf=mesh.ntriang
-    V=np.zeros(sum(surf))
+    surf=obj.ntriang
     M=sum(surf)
-    V[:surf(0)]=V0(0)
-    for i in range(1,M-1):
-        V[surf(i-1):surf(i)]=V0(i)
+    V=np.zeros(M)
+    V[:surf[0]]=V0[0]
+    for i in range(1,len(surf)):
+        V[surf[i-1]:surf(i)]=V0[i]
     return V
 
-def potential(mesh: object, q: np.array, pts: np.array) -> np.array:
-    V=mat_func(pts,mesh.vertex,mesh.topol,q)
+def potential(obj: object, q: np.array, pts: np.array) -> np.array:
+    V=mat_func(pts,obj.vertex,obj.topol,q)
     V=V/4*pi*epsilon
     return V
