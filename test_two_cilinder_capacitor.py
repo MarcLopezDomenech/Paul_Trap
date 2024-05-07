aa0975357eef2d3d50d6ae277f2389285f9a85a0
«D0ddd6» <damiacanyameres@gmail.com>
2024-05-07 12:23:15 +0200

    Gràfiques convergència per cilindre 2D

--- test_two_cilinder_capacitor.py
+++ test_two_cilinder_capacitor.py
@@ -84,3 +84,105 @@ if __name__ == '__main__':
     # Plot vector field
     plt.quiver(X_downsampled, Y_downsampled, Ex_downsampled, Ey_downsampled, scale = 1)
     plt.show()
+
+
+    #We redo the calculations for 2N, to check how error changes
+    mesh1, h = create_cilinder_mesh(2*N, c1, r1)
+
+    #create second cilinder
+    mesh2, h = create_cilinder_mesh(2*N, c2, r2)
+
+    #merge both meshes in one
+    mesh = np.zeros((2*2*N, 2))
+    mesh[:2*N, :] = mesh1
+    mesh[2*N:, :] = mesh2
+
+    V0 = 1
+    V = np.zeros(2*2*N)
+    V[:2*N] = V0 / 2
+    V[2*N:] = -V0 / 2
+    q = linear_system(mesh, h, V)
+
+    C = h * sum(q[:2*N]) / V0
+    print('Capacitance for 2N (SI):', C)
+
+    E2 = abs(C-C_teo)
+    r2 = E2/C_teo
+    print('Absolute Error for 2N(SI):', E2)
+    print('Relative Error for 2N:', r2)
+    print('Quocient errors absoluts:', E/E2)
+    print('Quocient errors relatius:', r/r2)
+
+    
+    #Study of the variation of the error with N
+    E_all=[]
+    QE_all=[]
+    r_all=[]
+    Qr_all=[]
+    N_all=[]
+    for N in range(100, 400, 20):
+        N_all.append(N)
+        c1 = np.array([-1, 0]) #centre first cilinder
+        r1 = 0.5 #radius first cilinder
+        mesh1, h = create_cilinder_mesh(N, c1, r1)
+
+        #create second cilinder
+        c2 = np.array([1, 0]) #centre second cilinder
+        r2 = 0.5 #radius second cilinder
+        mesh2, h = create_cilinder_mesh(N, c2, r2)
+
+        #merge both meshes in one
+        mesh = np.zeros((2*N, 2))
+        mesh[:N, :] = mesh1
+        mesh[N:, :] = mesh2
+
+        V0 = 1
+        V = np.zeros(2*N)
+        V[:N] = V0 / 2
+        V[N:] = -V0 / 2
+        q = linear_system(mesh, h, V)
+
+        C = h * sum(q[:N]) / V0
+
+        C_teo = np.pi*epsilon/np.arccosh(np.linalg.norm(c1)/r1) #Only works if c1=c2 and r1=r2
+        E = abs(C-C_teo)
+        r = E/C_teo
+        E_all.append(E)
+        r_all.append(r)
+
+        #We redo for 2N
+        mesh1, h = create_cilinder_mesh(2*N, c1, r1)
+
+        #create second cilinder
+        mesh2, h = create_cilinder_mesh(2*N, c2, r2)
+
+        #merge both meshes in one
+        mesh = np.zeros((2*2*N, 2))
+        mesh[:2*N, :] = mesh1
+        mesh[2*N:, :] = mesh2
+
+        V0 = 1
+        V = np.zeros(2*2*N)
+        V[:2*N] = V0 / 2
+        V[2*N:] = -V0 / 2
+        q = linear_system(mesh, h, V)
+
+        C = h * sum(q[:2*N]) / V0
+        E2 = abs(C-C_teo)
+        r2 = E2/C_teo
+        QE_all.append(E/E2)
+        Qr_all.append(r/r2)
+
+    plt.plot(N_all, E_all, '.--', label='Error absolut')
+    plt.legend()
+    plt.show()
+    plt.plot(N_all, r_all, '.--', label='Error relatiu')
+    plt.legend()
+    plt.show()
+    plt.plot(N_all, QE_all, '.--', label='Quocient error absolut amb error 2N')
+    plt.plot(N_all, Qr_all, '.--', label='Quocient error relatiu amb error 2N')
+    plt.legend()
+    plt.show()
+        
+
+
