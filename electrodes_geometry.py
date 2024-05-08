d9915f0c346e52ea39a64a95963b4a97701e84c0
Marc López <marc.lopez.domenech@estudiantat.upc.edu>
2024-05-08 16:55:29 +0200

    electrode

--- electrodes_geometry.py
+++ electrodes_geometry.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial import Delaunay
 from functions3D import obj, plot_mesh
+import math as m
+import pyvista as pv
 
 def one_sheet_hyperboloid(deltaz, zlim, r):
     z = np.linspace(-zlim, zlim, int(1/deltaz))
@@ -28,5 +30,34 @@ def one_sheet_hyperboloid(deltaz, zlim, r):
     object = obj(points_3d, tri.simplices, bari, ntriang)
     return object
 
+def hiper(radius: float, center: np.array,num_points: int,circum:int):
+    index=radius
+    dist =float(radius) / float(circum)
+    points=[]
+    num = 0
+    for i in range(circum):
+        num=num+2*m.pi*(i+1)*dist
+    dens=float(num_points/num)
+    while index > 0:
+        numero=int(dens*2*m.pi*index)
+        for j in range(numero):
+             x=index*m.cos(float(2*m.pi*j)/float(numero))
+             y=index*m.sin(float(2*m.pi*j)/float(numero))
+             z=m.sqrt(m.pow(x,2)+m.pow(y,2) +1)
+             points.append(center+np.array([x,y,z]))
+        index=index-dist
+        points.append([0,0,1])
+    return points
+
 object = one_sheet_hyperboloid(0.1, 0.5, 1)
 plot_mesh(object, 'One-sheet hyperboloid')
+
+points=hiper(10.0,np.array([0,0,0]),5000,100)
+X, Y, Z = zip(*points)
+ax = plt.axes(projection ='3d')
+ax.scatter(X, Y, Z, 'green')
+plt.show()
+
+cloud = pv.PolyData(points)
+surf = cloud.delaunay_2d()
+surf.plot(show_edges=True)
