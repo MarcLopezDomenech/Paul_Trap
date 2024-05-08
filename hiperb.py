974a025961fd7b36c4a72e855f8d0ccd3a7af51a
Marc López <marc.lopez.domenech@estudiantat.upc.edu>
2024-05-08 12:24:23 +0200

    merged

--- hiperb.py
+++ hiperb.py
@@ -43,26 +43,24 @@ def lateral(radius: float, center: np.array,num_points: int,altura:float, circum
     return points
 
 def lateral1(center: np.array,num_points: int,altura:float, circum:int):
-    dist =float(altura) / float(circum)
-    index=altura/2
+    dist =float(altura/2) / float(circum)
+    index=0
     points=[]
     num = 0
     for i in range(circum):
-        alt=i*dist-(altura/2)
+        alt=i*dist
         rad=m.sqrt(m.pow(alt,2) +1)
         num=num+2*m.pi*rad
     dens=float(num_points/num)
-    while index > -altura/2:
+    while abs(index) < abs(altura/2):
         rad=m.sqrt(m.pow(index,2) +1)
         numero=int(dens*2*m.pi*abs(rad))
         for j in range(numero):
             x=rad*m.cos(float(2*m.pi*j)/float(numero))
             y=rad*m.sin(float(2*m.pi*j)/float(numero))
             z=index
-            if index<0:
-                z=-z
             points.append(center+np.array([x,y,z]))
-        index=index-dist
+        index=index+dist
     return points
 
 points=hiper(10.0,np.array([0,0,0]),1000,100)
@@ -82,8 +80,22 @@ fig = plt.figure()
 ax = plt.axes(projection ='3d')
 ax.scatter(x, y, z, 'green')
 plt.show()
+
+points1=lateral1(np.array([0,0,0]),1000,-10,100)
+x, y, z = zip(*points1)
+fig = plt.figure()
+ax = plt.axes(projection ='3d')
+ax.scatter(x, y, z, 'green')
+plt.show()
+
+
 cloud = pv.PolyData(points)
 surf = cloud.delaunay_2d()
-surf.plot(show_edges=True)
-surf.plot(cpos="yz", show_edges=True)
-surf.plot(cpos="xy", show_edges=True)
+
+cloud = pv.PolyData(points1)
+surf1 = cloud.delaunay_2d()
+
+merged = surf.merge(surf1)
+merged.plot(show_edges=True)
+merged.plot(cpos="yz", show_edges=True)
+merged.plot(cpos="xy", show_edges=True)
