75dd4191041b5a96b546b30c3f0b1dd6aa02cf98
Marc López <marc.lopez.domenech@estudiantat.upc.edu>
2024-05-08 14:54:16 +0200

    hiper fin

--- hiperb.py
+++ hiperb.py
@@ -3,23 +3,18 @@ import math as m
 import matplotlib.pyplot as plt
 import pyvista as pv
 
-def hiper(radius: float, center: np.array,num_points: int,circum:int):
-    index=radius
-    dist =float(radius) / float(circum)
-    points=[]
-    num = 0
-    for i in range(circum):
-        num=num+2*m.pi*(i+1)*dist
-    dens=float(num_points/num)
-    while index > 0:
-        numero=int(dens*2*m.pi*index)
-        for j in range(numero):
-             x=index*m.cos(float(2*m.pi*j)/float(numero))
-             y=index*m.sin(float(2*m.pi*j)/float(numero))
-             z=m.sqrt(m.pow(x,2)+m.pow(y,2) +1)
-             points.append(center+np.array([x,y,z]))
-        index=index-dist
-    return points
+def two_sheets_hyperboloid(ulim):
+    n = 100
+    u = np.linspace(-ulim, ulim, n)
+    u_ = np.array([u for _ in range(n)])
+    u = u_
+    v = np.linspace(0, 2*np.pi, n)
+    v_ = np.array([v for _ in range(n)])
+    v = v_.T
+    x = np.sinh(u)*np.cos(v)
+    y = np.sinh(u)*np.sin(v)
+    z = np.cosh(u)
+    return x, y, z
 
 def lateral(radius: float, center: np.array,num_points: int,altura:float, circum:int):
     dist =float(altura) / float(circum)
@@ -63,8 +58,8 @@ def lateral1(center: np.array,num_points: int,altura:float, circum:int):
         index=index+dist
     return points
 
-points=hiper(10.0,np.array([0,0,0]),1000,100)
-x, y, z = zip(*points)
+X, Y, Z = two_sheets_hyperboloid(3)
+points = np.c_[X.reshape(-1), Y.reshape(-1), Z.reshape(-1)]
 fig = plt.figure()
 ax = plt.axes(projection ='3d')
 ax.plot3D(x, y, z, 'green')
@@ -91,10 +86,8 @@ plt.show()
 
 cloud = pv.PolyData(points)
 surf = cloud.delaunay_2d()
-
 cloud = pv.PolyData(points1)
 surf1 = cloud.delaunay_2d()
-
 merged = surf.merge(surf1)
 merged.plot(show_edges=True)
 merged.plot(cpos="yz", show_edges=True)
