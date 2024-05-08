5759340133520cc0c9c37d16b4ab4a11900948ac
Marc López <marc.lopez.domenech@estudiantat.upc.edu>
2024-05-08 11:10:48 +0200

    hyper

--- hiperb.py
+++ hiperb.py
@@ -14,7 +14,10 @@ def hiper(radius: float, center: np.array,num_points: int,circum:int):
     while index > 0:
         numero=int(dens*2*m.pi*index)
         for j in range(numero):
-            points.append(center+np.array([index*m.cos(float(2*m.pi*j)/float(numero)),index*m.sin(float(2*m.pi*j)/float(numero)),1/m.cosh(index)]))
+             x=index*m.cos(float(2*m.pi*j)/float(numero))
+             y=index*m.sin(float(2*m.pi*j)/float(numero))
+             z=m.sqrt(m.pow(x,2)+m.pow(y,2) +1)
+             points.append(center+np.array([x,y,z]))
         index=index-dist
     return points
 
@@ -28,13 +31,13 @@ def lateral(radius: float, center: np.array,num_points: int,altura:float, circum
     dens=float(num_points/num)
     while index > -altura/2:
         rad=radius*m.cosh(m.dist([radius,0,index], [0,0,0])-radius)
-        numero=int(dens*2*m.pi*abs(index))
+        numero=int(dens*2*m.pi*abs(rad))
         for j in range(numero):
             points.append(center+np.array([rad*m.cos(float(2*m.pi*j)/float(numero)),rad*m.sin(float(2*m.pi*j)/float(numero)),index]))
         index=index-dist
     return points
     
-points=hiper(10.0,np.array([0,0,0]),3000,50)
+points=hiper(10.0,np.array([0,0,0]),1000,100)
 x, y, z = zip(*points)
 fig = plt.figure()
 ax = plt.axes(projection ='3d')
@@ -45,11 +48,11 @@ surf = cloud.delaunay_2d()
 surf.plot(show_edges=True)
 surf.plot(cpos="yz", show_edges=True)
 
-points=lateral(20.0,np.array([0,0,0]),10000,10,10)
+points=lateral(20.0,np.array([0,0,0]),10000,10,20)
 x, y, z = zip(*points)
 fig = plt.figure()
 ax = plt.axes(projection ='3d')
-ax.plot3D(x, y, z, 'green')
+ax.scatter(x, y, z, 'green')
 plt.show()
 cloud = pv.PolyData(points)
 surf = cloud.delaunay_2d()
